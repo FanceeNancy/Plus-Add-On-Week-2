d806d3d260ce5032a2aa1f30bc9344e780610220
FanceeNancy <nancy.melchert@gmail.com>
2024-11-29 13:59:51 -0500

    added more functionality: -added more cities to the drop down -when city is selected, the time will appear in blue with a box around it (injected html into div)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function updateTime() {
     let firstCityDateElement = firstCityElement.querySelector(".date");
     let firstCityTimeElement = firstCityElement.querySelector(".time");
     let fullCityName = pprintCityName(firstCityElement.id);
+    //console.log(fullCityName);
     let timezone = getTimezoneFromCity(fullCityName);
     let firstCityTime = moment().tz(`${timezone}`);
     let cityTime = firstCityTime.format("h:mm:ss");
@@ -17,7 +18,7 @@ function updateTime() {
 }
 
 function getTimezoneFromCity(cityName) {
-  let zones = moment.tz.names();
+  const zones = moment.tz.names();
   for (const zone of zones) {
     if (zone.includes(cityName)) {
       return zone;
@@ -27,6 +28,21 @@ function getTimezoneFromCity(cityName) {
 }
 
 function pprintCityName(cityNameWithAZ) {
+  if (cityNameWithAZ.includes("-")) {
+    let cityNameFirstPart = cityNameWithAZ.split("-")[0];
+    let updatedFirstPart = pprintOneName(cityNameFirstPart);
+    let cityNameSecondPart = cityNameWithAZ.split("-")[1];
+    let updatedSecondPart = pprintOneName(cityNameSecondPart);
+    //console.log(`${updatedFirstPart} ${updatedSecondPart}`);
+    return `${updatedFirstPart}_${updatedSecondPart}`;
+  } else {
+    let updatedCityName = pprintOneName(cityNameWithAZ);
+    //console.log(updatedCityName);
+    return updatedCityName;
+  }
+}
+
+function pprintCityNameTwo(cityNameWithAZ) {
   if (cityNameWithAZ.includes("-")) {
     let cityNameFirstPart = cityNameWithAZ.split("-")[0];
     let updatedFirstPart = pprintOneName(cityNameFirstPart);
@@ -46,8 +62,30 @@ function pprintOneName(cityNameWithAZ) {
   for (let i = 1; i < cityName.length; i++) {
     endOfTheName += cityName[i];
   }
-  return `${firstLetter}${endOfTheName}`;
+  let updatedName = `${firstLetter}${endOfTheName}`;
+  return updatedName;
+}
+
+function updateCity(event) {
+  console.log(event.target.value);
+  let fullCityName = pprintCityNameTwo(event.target.value);
+  let timezone = getTimezoneFromCity(fullCityName);
+  console.log(timezone);
+  let citiesElement = document.querySelector("#chosen-city");
+  console.log(citiesElement);
+  citiesElement.innerHTML = `
+  <div class="city" id="${event.target.value}">
+            <div>
+              <h2>${fullCityName}</h2>
+              <div class="date"></div>
+            </div>
+            <div class="time"></div>
+          </div>`;
+  citiesElement.classList.add("show");
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", updateCity);
